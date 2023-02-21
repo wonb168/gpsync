804876c364b4cf3794064696a99ce27ce90196af
wonb168 <37640893@qq.com>
2023-02-21 16:19:38 +0800

    gpsync v0.2

--- gpsync.py
+++ gpsync.py
@@ -1,8 +1,78 @@
 # 加# %%方便单步调试，正式执行时替换为##%%，需要单步时还原为# %%
 # %%
-import os
+import os,sys
 import re 
+import tomli
+import json
+import psycopg2
+from pandas import json_normalize
 
+# %%
+# 由yaml配置文件生成include table的json、exclude table的txt以及include table的delete sql
+
+def gen_copyfile(cfg_file='config.toml'):
+    # exclude table file：db.schema.table
+    with open(cfg_file, "rb") as f:
+        toml_dict = tomli.load(f)
+    df = json_normalize(toml_dict)#扁平化json数据
+    # 写exclude文件
+    str = '\n'
+    print('generate exclude_table.txt...')
+    with open("exclude_table.txt","w") as f:
+        f.write(str.join(df.columns.tolist()))
+    dct_json=[]
+    dels=[]
+    for r in df.columns.tolist():
+        row=f'delete from {r} {df[r][0]};\n'
+        # print(row)
+        dels.append(row)
+        dct={}
+        dct['source']=r
+        dct['sql']=f"select * from {r} {df[r][0]}"
+        dct['dest']=r
+        dct_json.append(dct)
+    print('generate include_table.json...')
+    with open("include_table.json","w") as f:
+        f.write(json.dumps(dct_json))
+    return dels
+
+# %%
+def run_sql(sql,dest,dbname):
+    conn = psycopg2.connect(database=dbname, user=dest['usr'],	password=dest['pwd'], host=dest['host'], port=dest['port'])
+    cur = conn.cursor()
+    cur.execute(sql)
+    conn.commit()
+    conn.close()
+    
+# %%
+# use gpcopy to transfer data
+def copy_data(source,dest,dbname):
+    # 增量用json文件，同时先delete表数据
+    dels='\n'.join(gen_copyfile())  
+    print('delete in dest db:\n',dels)
+
+    run_sql(dels,dest,dbname)
+    cmd=f"""export PGSSLMODE=disable && export PGPASSWORD={source['pwd']} && gpcopy \
+--source-host {source['host']} --source-port {source['port']} --source-user {source['usr']} \
+--dest-host {dest['host']} --dest-port {dest['port']} --dest-user {dest['usr']} \
+--include-table-json include_table.json --append
+"""
+    print(cmd)
+    os.system(cmd)
+    # 全量用exclude
+    cmd=f"""export PGSSLMODE=disable && export PGPASSWORD={source['pwd']} && gpcopy \
+--source-host {source['host']} --source-port {source['port']} --source-user {source['usr']} \
+--dest-host {dest['host']} --dest-port {dest['port']} --dest-user {dest['usr']} \
+--dbname {dbname} --exclude-table-file exclude_table.txt --truncate
+"""
+#     cmd="""export PGSSLMODE=disable && gpcopy --source-host {} --source-port {} --source-user {} \
+# --dest-host {} --dest-port {} --dest-user {} \
+# --dbname {} --exclude-table-file exclude_table.txt --truncate
+# """.format(source['host'],source['port'],source['usr'],dest['host'],dest['port'],dest['usr'],dbname) 
+    print(cmd)
+    os.system(cmd)
+    
+    
 # %%
 # 导出dump文件并预处理
 def dump(dbinfo,db,filename):
@@ -55,21 +125,33 @@ def split_dump(file_path):
     REVOKE FROM
     GRANT FROM
 """
-def del_dest(dump_dest,dump_src):
-    dels=[elem for elem in dump_dest if elem not in dump_src]
-    print('need del in dest db:',len(dels))
-    for o in dels:
-        print(o)
+def dest_has(dump_dest,dump_src):
+    # 若表空间不一致，直接退出
+    # compare_tablespace(dump_src,dump_dest)
+    has=[elem for elem in dump_dest if elem not in dump_src]
+    print('need del in dest db:',len(has))
+    dels=[]
+    for o in has:
+        print(o)        
         if (obj:=re.findall(r'^CREATE TABLE (\S+)', o)):
+            print(obj,len(obj))
             sql=f'drop table {obj[0]};'
             print(sql)
+            dels.append(sql)
         if (obj:=re.findall(r'^CREATE FUNCTION (.*?\))', o)):
             sql=f'drop function {obj[0]};'
             print(sql)
+            dels.append(sql)
         if (obj:=re.findall(r'^CREATE INDEX (\S+)', o)):
             sql=f'drop index {obj[0]};'
             print(sql)
-    return dels
+            dels.append(sql)
+        if (obj:=re.findall(r'^GRANT (.*+) TO (.*;)', o)):
+            sql=f'revoke {obj[0]} from {obj[1]};'
+            print(sql)
+            dels.append(sql)
+    # 目标库中执行dels
+    return dest_has
 
 # %%
 # 源库多的（源库新增或修改）
@@ -83,7 +165,7 @@ def src_adds(dump_src,dump_dest,dels):
         print(o)
 
 # %%
-def main(dbinfo,dbinfo2,dbname):
+def sync_schema(dbinfo,dbinfo2,dbname):
     filename=dbname+'_src.sql'
     filename2=dbname+'_dest.sql'
     dump(dbinfo,dbname,filename)
@@ -94,15 +176,19 @@ def main(dbinfo,dbinfo2,dbname):
     dump_dest=split_dump(filename2)
     print(len(dump_dest)) 
 
-    dels=del_dest(dump_dest,dump_src)
+    dels=dest_has(dump_dest,dump_src)
     src_adds(dump_src,dump_dest,dels)  
     
 # %%
-if __name__='__main__':
-    dbname='mdmaster_bsgj_dev551_product_dev'
+if __name__=='__main__':
+    dbname='mdmaster_platform'#'mdmaster_bsgj_dev551_product_dev'
     dbinfo={"host":"192.168.200.101", "port":2345, "usr":"gpadmin", "pwd":"密码"}
     dbinfo2={"host":"192.168.200.73", "port":2345, "usr":"gpadmin", "pwd":"密码"}
-    main(dbinfo,dbinfo2,dbname)
+    print('begint to sync schema...')
+    # sync_schema(dbinfo,dbinfo2,dbname)
+    print('begint to sync data...')
+    copy_data(dbinfo,dbinfo2,dbname)
+
 
 # %%
 
